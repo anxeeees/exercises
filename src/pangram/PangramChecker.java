49e916051f14eb29eb46ee23d1070224d784b7b2
Ester Stankovska <stankovskaester@gmail.com>
2023-12-04 19:46:10 +0100

    pangram, characters.

---
+++ src/pangram/PangramChecker.java
@@ -0,0 +1,27 @@
+package pangram;
+
+public class PangramChecker {
+
+    public boolean isPangram(String input) {
+        boolean[] alphabetPresent = new boolean[26];
+
+        for (char currentChar : input.toLowerCase().toCharArray()) {
+            if (Character.isLetter(currentChar)) {
+                alphabetPresent[currentChar - 'a'] = true;
+            }
+        }
+
+        for (boolean letter : alphabetPresent) {
+            if (!letter) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        PangramChecker pangramChecker = new PangramChecker();
+        System.out.println(pangramChecker.isPangram("The quick brown fox jumps over the lazy dog."));
+    }
+}
--- src/squeakyclean/SqueakyClean.java
+++ src/squeakyclean/SqueakyClean.java
@@ -1,15 +1,14 @@
 package squeakyclean;
 
-public class SqueakyClean {
-
+import java.util.regex.Pattern;
+class SqueakyClean {
     static String clean(String identifier) {
-        return identifier.replace(' ','_');
+        return Pattern.compile("-(\\p{IsAlphabetic})")//looking for -
+                .matcher(identifier)
+                .replaceAll(m -> m.group().toUpperCase())//replacing the word after - with Upper Case
+                .replaceAll("\s", "_") // all spaces with _
+                .replaceAll("\\p{Cntrl}", "CTRL")
+                .replaceAll("[^_\\p{IsAlphabetic}]", "") // deletes everything that is not letter
+                .replaceAll("[α-ω]", "");
     }
-
-    public static void main(String[] args) {
-        System.out.println(SqueakyClean.clean("my   Id"));
-
-    }
-
-
 }
