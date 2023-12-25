1071c141305e4b8fb6ccf050684b0ead728aada8
Ester Stankovska <stankovskaester@gmail.com>
2023-12-26 00:36:55 +0100

    pyramid.

---
+++ src/loops/Pyramid.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        //Write a program to print pyramid
+        int rows = 5;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rows - i - 1; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
