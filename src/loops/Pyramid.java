141df84d0ab69c6aec688213928cf58b12a8dd3e
Ester Stankovska <stankovskaester@gmail.com>
2023-12-26 22:23:41 +0100

    pyramid.

--- src/loops/Pyramid.java
+++ src/loops/Pyramid.java
@@ -5,9 +5,20 @@ public class Pyramid {
         //Write a program to print pyramid
         int rows = 5;
         for (int i = 0; i < rows; i++) {
+            System.out.println(" ");
             for (int j = 0; j < rows - i - 1; j++) {
                 System.out.print("  ");
             }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        //Write a program to print pyramid
+        for (int i = 0; i < rows; i++) {
+            System.out.println(" ");
             for (int k = 0; k < 2 * i + 1; k++) {
                 System.out.print("* ");
             }
