455e4d07af3ff94822c28fa24ff389c1e6a2cc46
Ester Stankovska <stankovskaester@gmail.com>
2024-01-01 01:58:13 +0100

    harder for loop.

--- src/loops/PyramidExc.java
+++ src/loops/PyramidExc.java
@@ -5,19 +5,19 @@ public class PyramidExc {
         int rows = 5;
 
         for (int i = 1; i <= rows; i++) { // 1, 2, 3, 4, 5
-            for (int j = 1; j <= i - rows; j--) { //4 3 2 1 0 space
+            for (int j = 1; j <= i - rows; j--) { // i - 5 = - 4 -> space 4 3 2 1 0
                 System.out.print("  ");
             }
 
-            for (int k = 1; k <= i; k++) {
+            for (int k = 1; k <= i; k++) { // i = 1, k = 1
                 System.out.print(".");
             }
 
-            for (int l = i + 1; l <= rows - i + 5; l++) {
+            for (int l = i ; l <= rows - i + 5; l++) { // space from the first .
                 System.out.print(" ");
             }
 
-            for (int m = 1; m <= i; m++) {
+            for (int m = 1; m <= i; m++) { //1
                 System.out.print(".");
             }
 
