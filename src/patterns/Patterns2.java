8d17d1c00c6457972ff5fc7fab09b50b3209fcc3
Ester Stankovska <stankovskaester@gmail.com>
2024-01-01 22:52:39 +0100

    pyramid patterns.

--- src/patterns/Patterns2.java
+++ src/patterns/Patterns2.java
@@ -14,7 +14,44 @@ public class Patterns2 {
             }
             System.out.println();
 
+        }
+
+        System.out.println();
+        // REVERSED
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j < i; j++) { // space at the start <= i
+                System.out.print("  "); // double space because of the space at the star
+                // other way is "" and "*"
+            }
+            for (int k = 4; k >= i; k--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        // combination
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 3; j >= i; j--) {
+                System.out.print("  "); // double space because of the space at the star
+                // other way is "" and "*"
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
 
         }
+        for (int i = 2; i <= 4; i++) {
+            for (int j = 1; j < i; j++) { // space at the start <= i
+                System.out.print("  "); // double space because of the space at the star
+                // other way is "" and "*"
+            }
+            for (int k = 4; k >= i; k--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
     }
 }
