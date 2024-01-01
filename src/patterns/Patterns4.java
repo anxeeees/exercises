ee6644cb389b66192a00a28bd02fa38720626186
Ester Stankovska <stankovskaester@gmail.com>
2024-01-02 00:32:00 +0100

    pyramid patterns.

--- src/patterns/Patterns4.java
+++ src/patterns/Patterns4.java
@@ -13,5 +13,16 @@ public class Patterns4 {
             }
             System.out.println();
         }
+        System.out.println();
+        // 2
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" "); // double space
+            }
+            for (int k = 9; k > (i * 2); k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
