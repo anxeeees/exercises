7c927c78599c6e4c01a4149c42f910e50efd0ca6
Ester Stankovska <stankovskaester@gmail.com>
2024-02-10 18:45:39 +0100

    copying old array to new array.

--- src/arrays/Arr4.java
+++ src/arrays/Arr4.java
@@ -9,13 +9,24 @@ public class Arr4 {
 
         int[] array = {25, 14, 18, 15, 36, 56, 77, 18, 29, 50};
         System.out.println(Arrays.toString(array));
-        remove(array, 5);
+        remove(array, 1);
+
+
+        //8. Write a Java program to copy an array by iterating old array.
+
+        int[] new_array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            new_array[i] = array[i];
+        }
+        System.out.println(Arrays.toString(new_array));
+
+
     }
 
     // Remove method
     static void remove(int arr[], int index) {
         int i;
-        for ( i = index; i < arr.length - 1; i++) {
+        for (i = index; i < arr.length - 1; i++) {
             arr[i] = arr[i + 1];
         }
         arr[i] = 0;
